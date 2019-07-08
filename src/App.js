42df26e15a7c4ef61e851e18e57d880c77057207
fxola <afolabiabass62@gmail.com>
2019-07-08 10:32:43 +0100

    Chore(webpack): Setup webpack configuration for project - install necessary webpack and react dependencies - write webpack config for development and production environments - write entry file - finishes [#167116061]

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+const App = () => {
+  return <h1>Wel come To Banka!!!</h1>;
+};
+
+export default hot(module)(App);
---
+++ webpack.config.base.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: [
+            'react-hot-loader/babel',
+            '@babel/plugin-proposal-class-properties'
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
